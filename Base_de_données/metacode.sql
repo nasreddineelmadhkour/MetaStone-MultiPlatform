bd7e682461e1921c274ede8883c3becdf4ee255e
Nasreddine El Madhkour <nasreddine1234@gmail.com>
2022-02-10 01:12:03 +0100

    Update metacode.sql
    -Update semi final base de données commun .

--- Base_de_données/metacode.sql
+++ Base_de_données/metacode.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 09 fév. 2022 à 20:25
+-- Généré le : jeu. 10 fév. 2022 à 01:09
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 7.4.15
 
@@ -218,21 +218,28 @@ CREATE TABLE `tournoi` (
 
 CREATE TABLE `utilisateur` (
   `ID_utilisateur` int(11) NOT NULL,
-  `Nom` varchar(20) NOT NULL,
-  `Prénom` varchar(20) NOT NULL,
+  `Nom` varchar(255) NOT NULL,
+  `Prénom` varchar(255) NOT NULL,
   `Date_naiss` date NOT NULL,
-  `E_mail` varchar(40) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `Tel` int(14) NOT NULL,
   `Adresse` varchar(40) NOT NULL,
   `Nom_utilisateur` varchar(20) NOT NULL,
   `Mot_de_passe` varchar(50) NOT NULL,
-  `Sexe` int(1) NOT NULL,
+  `Sexe` varchar(255) NOT NULL,
   `Photo_de_profil` varchar(80) NOT NULL,
   `MTC` int(11) NOT NULL,
-  `Rôle` int(1) NOT NULL,
-  `Rank` varchar(11) NOT NULL,
+  `Rôle` varchar(255) NOT NULL,
+  `Rank` varchar(255) NOT NULL,
   `Id_consommateur` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`ID_utilisateur`, `Nom`, `Prénom`, `Date_naiss`, `email`, `Tel`, `Adresse`, `Nom_utilisateur`, `Mot_de_passe`, `Sexe`, `Photo_de_profil`, `MTC`, `Rôle`, `Rank`, `Id_consommateur`) VALUES
+(1, 'nasr', 'madhkour', '1998-04-12', 'nad@dd', 15545, 'daz', 'daz', 'daz', 'Homme', 'dzad', 544, 'ADMIN', 'Hero', 0);
 
 --
 -- Index pour les tables déchargées
@@ -248,25 +255,31 @@ ALTER TABLE `carte`
 -- Index pour la table `collection`
 --
 ALTER TABLE `collection`
-  ADD PRIMARY KEY (`Id_collection`);
+  ADD PRIMARY KEY (`Id_collection`),
+  ADD KEY `etranger_collection_user` (`Id_utilisateur`),
+  ADD KEY `etranger_collection_carte` (`Id_carte`);
 
 --
 -- Index pour la table `commande`
 --
 ALTER TABLE `commande`
-  ADD PRIMARY KEY (`Id_commande`);
+  ADD PRIMARY KEY (`Id_commande`),
+  ADD KEY `etranger_cammande_user` (`Id_utilisateur`);
 
 --
 -- Index pour la table `commentaire`
 --
 ALTER TABLE `commentaire`
-  ADD PRIMARY KEY (`Id_cmnt`);
+  ADD PRIMARY KEY (`Id_cmnt`),
+  ADD KEY `etranger_user_comnt` (`Id_utilisateur`),
+  ADD KEY `etranger_poste_comnt` (`Id_poste`);
 
 --
 -- Index pour la table `demande`
 --
 ALTER TABLE `demande`
-  ADD PRIMARY KEY (`Id_demande`);
+  ADD PRIMARY KEY (`Id_demande`),
+  ADD KEY `etranger_demande` (`Id_utilisateur`);
 
 --
 -- Index pour la table `evénement`
@@ -278,7 +291,9 @@ ALTER TABLE `evénement`
 -- Index pour la table `joueur`
 --
 ALTER TABLE `joueur`
-  ADD PRIMARY KEY (`Id_joueur`);
+  ADD PRIMARY KEY (`Id_joueur`),
+  ADD KEY `etranger_joueur_id_User` (`Id_utilisateur`),
+  ADD KEY `etranger_ID_Tournoi` (`Id_tournoi`);
 
 --
 -- Index pour la table `merche`
@@ -290,25 +305,31 @@ ALTER TABLE `merche`
 -- Index pour la table `panier`
 --
 ALTER TABLE `panier`
-  ADD PRIMARY KEY (`Id_panier`);
+  ADD PRIMARY KEY (`Id_panier`),
+  ADD KEY `etranger_produit_id` (`Id_produit`),
+  ADD KEY `etranger_cammande_id` (`Id_commande`);
 
 --
 -- Index pour la table `participants`
 --
 ALTER TABLE `participants`
-  ADD PRIMARY KEY (`Id_participant`);
+  ADD PRIMARY KEY (`Id_participant`),
+  ADD KEY `etranger_participant_idUser` (`Id_utilisateur`),
+  ADD KEY `etranger_participant_idseance` (`Id_séance`);
 
 --
 -- Index pour la table `poste`
 --
 ALTER TABLE `poste`
-  ADD PRIMARY KEY (`Id_poste`);
+  ADD PRIMARY KEY (`Id_poste`),
+  ADD KEY `etranger_user_poste` (`Id_utilisateur`);
 
 --
 -- Index pour la table `séance`
 --
 ALTER TABLE `séance`
-  ADD PRIMARY KEY (`Id_séance`);
+  ADD PRIMARY KEY (`Id_séance`),
+  ADD KEY `etranger_seance_idUser` (`Id_coach`);
 
 --
 -- Index pour la table `tournoi`
@@ -321,7 +342,7 @@ ALTER TABLE `tournoi`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`ID_utilisateur`),
-  ADD UNIQUE KEY `E_mail` (`E_mail`),
+  ADD UNIQUE KEY `E_mail` (`email`),
   ADD UNIQUE KEY `Nom_utilisateur` (`Nom_utilisateur`);
 
 --
@@ -356,7 +377,7 @@ ALTER TABLE `commentaire`
 -- AUTO_INCREMENT pour la table `demande`
 --
 ALTER TABLE `demande`
-  MODIFY `Id_demande` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_demande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `evénement`
@@ -410,7 +431,70 @@ ALTER TABLE `tournoi`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `ID_utilisateur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `collection`
+--
+ALTER TABLE `collection`
+  ADD CONSTRAINT `etranger_collection_carte` FOREIGN KEY (`Id_carte`) REFERENCES `carte` (`Id_carte`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `etranger_collection_user` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `etranger_cammande_user` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `etranger_poste_comnt` FOREIGN KEY (`Id_poste`) REFERENCES `poste` (`Id_poste`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `etranger_user_comnt` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `demande`
+--
+ALTER TABLE `demande`
+  ADD CONSTRAINT `etranger_demande` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  ADD CONSTRAINT `etranger_ID_Tournoi` FOREIGN KEY (`Id_tournoi`) REFERENCES `tournoi` (`Id_tournoi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `etranger_joueur_id_User` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD CONSTRAINT `etranger_cammande_id` FOREIGN KEY (`Id_commande`) REFERENCES `commande` (`Id_commande`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `etranger_produit_id` FOREIGN KEY (`Id_produit`) REFERENCES `merche` (`Id_produit`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `participants`
+--
+ALTER TABLE `participants`
+  ADD CONSTRAINT `etranger_participant_idUser` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `etranger_participant_idseance` FOREIGN KEY (`Id_séance`) REFERENCES `séance` (`Id_séance`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `poste`
+--
+ALTER TABLE `poste`
+  ADD CONSTRAINT `etranger_user_poste` FOREIGN KEY (`Id_utilisateur`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `séance`
+--
+ALTER TABLE `séance`
+  ADD CONSTRAINT `etranger_seance_idUser` FOREIGN KEY (`Id_coach`) REFERENCES `utilisateur` (`ID_utilisateur`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

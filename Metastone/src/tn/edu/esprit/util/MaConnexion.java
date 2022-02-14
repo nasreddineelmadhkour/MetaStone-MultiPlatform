310f9664d00202b0ddc57ebab24e65a1251c2854
Saif Eddine Layouni <33223873+saiflayouni@users.noreply.github.com>
2022-02-14 20:08:22 +0100

    connexion etablie

---
+++ Metastone/src/tn/edu/esprit/util/MaConnexion.java
@@ -0,0 +1,57 @@
+
+
+package tn.edu.esprit.util;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author admin
+ */
+public class MaConnexion {
+    // BD Credentials
+    final static String URL= "jdbc:mysql://127.0.0.1:3306/pidev";
+    final static String USERNAME="root";
+    final static String PWD="";
+    //att
+    private Connection cnx;
+    
+    //Singleton #1
+    static MaConnexion instance=null;
+    //constructor
+    //Singleton #2
+
+    private MaConnexion() {
+       
+        try {
+            cnx= (Connection) DriverManager.getConnection(URL,USERNAME,PWD);
+            System.out.println("Connexion etablie avec Succes");
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    
+    //getters & setters
+
+    public Connection getCnx() {
+        return cnx;
+    }
+    
+    //Singleton #3
+    public static MaConnexion getInstance() {
+        if(instance==null){
+            instance=new MaConnexion();
+        }
+        return instance;
+    }
+    
+
+    
+}
+
+
